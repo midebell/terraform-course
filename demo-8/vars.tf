fd1c3b16a6dbea129c5ac6c4bcae3e03f83a8414
Olumide <midebell@example.com>
2018-07-31 18:13:36 +0000

    new 7-31

--- demo-8/vars.tf
+++ demo-8/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
@@ -10,8 +10,8 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+       us-east-1 = "ami-ef151d90"
+       us-west-2 = "ami-1cc69e64"
+       us-west-1 = "ami-d8b05cbb"
   }
 }
