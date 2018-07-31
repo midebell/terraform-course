61e316cbc68ae0b6151f9addfd0a1bae5b54cbc3
Olumide <midebell@example.com>
2018-07-31 00:02:22 +0000

    new edit 7-30

--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -1,14 +1,14 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-ef151d90"
+    us-west-2 = "ami-1cc69e64"
+    us-west-1 = "ami-d8b05cbb"
   }
 }
 
